3e199776a596b8bb32b6945031a3703191177fed
gak77109-art <gak77109@gmail.com>
2026-05-21 01:15:01 +0530

    fixed settings page

---
+++ app/settings/page.tsx
@@ -0,0 +1,12 @@
+export default function Settingspage() {
+    return (
+        <div className="min-h-screen p-6 text-white">
+            <h1 className="text-3x1 font-bold">
+                Settings
+            </h1>
+            <p className="mt-4 text-gray-400">
+                Settings Page coming soon.
+            </p>
+        </div>
+    );
+}
